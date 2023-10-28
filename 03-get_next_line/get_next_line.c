4583297325c8c7b08332963145e5c6b7228bc289
Aerly-Lex <117720936+Aerly-Lex@users.noreply.github.com>
2023-10-28 11:33:03 +0200

    Add files via upload

--- 03-get_next_line/get_next_line.c
+++ 03-get_next_line/get_next_line.c
@@ -6,129 +6,94 @@
 /*   By: Dscheffn <dscheffn@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 11:56:03 by Dscheffn          #+#    #+#             */
-/*   Updated: 2023/10/24 10:15:50 by Dscheffn         ###   ########.fr       */
+/*   Updated: 2023/10/28 11:32:21 by Dscheffn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+char	*reader(int fd, char *remainder)
 {
-	t_list	*buffer;
+	char	*buff;
+	int		bytes_reader;
 
-	buffer = *lst;
-	if (*lst == NULL)
+	bytes_reader = 1;
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (buff == NULL)
+		return (NULL);
+	while (bytes_reader > 0 && ft_strchr(remainder, '\n') == 0)
 	{
-		*lst = new;
-		return ;
-	}
-	while (buffer->next)
-	{
-		buffer = buffer->next;
-	}
-	buffer->next = new;
-}
-
-// int	ft_strlen_n(char *buffer)
-// {
-// 	int	counter;
-
-// 	counter = 0;
-// 	while (buffer[counter] != '\n' && buffer[counter] != '\0' )
-// 		counter++;
-// 	return (counter);
-// }
-
-void	*reader(int fd, t_list **head)
-{
-	char	*buffer;
-	char	*tmp;
-	int		byte_size;
-	int		i;
-	int		start;
-
-	i = 0;
-	start = 0;
-	while (buffer[i] != '\0')
-	{
-		byte_size = read(fd, buffer, BUFFER_SIZE);
-		j = 0;
-		tmp = ft_lstnew(tmp); //malloc(sizeof(char *) * ft_strlen_n(buffer) + 2);
-		if (!tmp)
-			return (0);
-		while (buffer[i + j] != '\n')
+		bytes_reader = read(fd, buff, BUFFER_SIZE);
+		if (bytes_reader == -1)
 		{
-			tmp[j] = buffer[j];
-			j++;
+			free(buff);
+			return (NULL);
 		}
-		tmp[j++] = '\n';
-		tmp[j++] = '\0';
-		head->content = tmp;
-		free(tmp);
-		i += j;
+		buff[bytes_reader] = '\0';
+		remainder = ft_strjoin(remainder, buff);
+		if (remainder == 0)
+			return (free(remainder), NULL);
 	}
+	free(buff);
+	return (remainder);
 }
 
 char	*get_next_line(int fd)
 {
-	static t_file	*head;
-	char			*line;
-	char			*tmp;
+	static char	*remainder;
+	char		*line;
 
-	if (head == 0)
-		reader(fd, &head);
-		// head = 0;
-		// head = malloc(sizeof(t_list));
-	if (!head)
-		return (0);
-
-	line = head->content;
-	tmp = head;
-	if (head->next == NULL)
-		free(head)
-	else
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	remainder = reader(fd, remainder);
+	if (remainder == NULL)
+		return (NULL);
+	line = line_grabber(remainder);
+	printf("line:%s\n", line);
+	remainder = remainder_refresh(remainder);
+	// printf("remainder:%s\n", remainder);
+	if (remainder != NULL && *remainder == '\0')
 	{
-		head = head->next;
-		free(tmp);
+		free(remainder);
+		remainder = NULL;
 	}
+	printf("remainder:%s\n", remainder);
 	return (line);
 }
 
-#include <fcntl.h>
 // return Number of bytes read on success
 // return 0 on reaching the end of file
 // return -1 on error
 // return -1 on signal interrupt
 
-	// line = ft_get_line(left_str);
-	// left_str = ft_new_left_str(left_str);
+// #include <fcntl.h>
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	*line;
+// 	int		i = 1;
+// 	// int		fd1;
+// 	// int		fd2;
+// 	int		fd3;
+// 	// fd1 = open("tests/test.txt", O_RDONLY);
+// 	// fd2 = open("tests/test1.txt", O_RDONLY);
+// 	fd3 = open("tests/test2.txt", O_RDONLY);
 
-int	main(void)
-{
-	char	*line;
-	int		i = 1;
-	// int		fd1;
-	// int		fd2;
-	int		fd3;
-	// fd1 = open("tests/test.txt", O_RDONLY);
-	// fd2 = open("tests/test1.txt", O_RDONLY);
-	fd3 = open("tests/test2.txt", O_RDONLY);
-
-	while (i < 8)
-	{
-	// 	// line = get_next_line(fd1);
-	// 	// printf("line1-%d: %s", i, line);
-	// 	// free(line);
-	// 	// line = get_next_line(fd2);
-	// 	// printf("line2-%d: %s", i, line);
-	// 	// free(line);
-		line = get_next_line(fd3);
-		printf("line3-%d: %s", i, line);
-		free(line);
-		i++;
-	}
-	// close(fd1);
-	// close(fd2);
-	close(fd3);
-	return (0);
-}
+// 	while (i < 2)
+// 	{
+// 		// line = get_next_line(fd1);
+// 		// printf("line1-%d: %s", i, line);
+// 		// free(line);
+// 		// line = get_next_line(fd2);
+// 		// printf("line2-%d: %s", i, line);
+// 		// free(line);
+// 		line = get_next_line(fd3);
+// 		// printf("line3-%d: %s", i, line);
+// 		free(line);
+// 		i++;
+// 	}
+// 	// close(fd1);
+// 	// close(fd2);
+// 	close(fd3);
+// 	return (0);
+// }
--- 03-get_next_line/get_next_line_utils.c
+++ 03-get_next_line/get_next_line_utils.c
@@ -6,37 +6,110 @@
 /*   By: Dscheffn <dscheffn@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 11:23:23 by Dscheffn          #+#    #+#             */
-/*   Updated: 2023/10/23 16:31:58 by Dscheffn         ###   ########.fr       */
+/*   Updated: 2023/10/28 11:32:20 by Dscheffn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+size_t	ft_strlen(const char *c)
 {
-	t_list	*current;
+	int	len;
 
-	current = *lst;
-	if (*lst == NULL)
-	{
-		*lst = new;
-		return ;
-	}
-	while (current->next)
-	{
-		current = current->next;
-	}
-	current->next = new;
+	len = 0;
+	if (c == NULL)
+		return (0);
+	while (c[len] != '\0')
+		len++;
+	return (len);
 }
 
-t_list	*ft_lstnew(void *content)
+char	*ft_strchr(char *s, int c)
 {
-	t_list	*str;
-
-	str = malloc(sizeof(t_list));
-	if (str == NULL)
+	if (s == NULL)
 		return (NULL);
-	str->content = content;
-	str->next = NULL;
-	return (str);
+	while (*s != '\0' && *s != (char)c)
+		s++;
+	if (*s == (char)c)
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new_s;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	counter;
+
+	counter = 0;
+	if (!s1 && !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new_s = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (new_s == NULL)
+		return (NULL);
+	while (counter++ < s1_len)
+		new_s[counter - 1] = s1[counter - 1];
+	counter = 0;
+	while (counter < s2_len)
+	{
+		new_s[s1_len + counter] = s2[counter];
+		counter++;
+	}
+	new_s[s1_len + s2_len] = '\0';
+	free(s1);
+	return (new_s);
+}
+
+char	*line_grabber(char *remainder)
+{
+	char	*line;
+	int		counter;
+
+	counter = 0;
+	if (remainder[counter] == '\0')
+		return (NULL);
+	while (remainder[counter] != '\0' && remainder[counter] != '\n')
+		counter++;
+	if (remainder[counter] == '\n')
+		counter++;
+	line = malloc(sizeof(char) * (counter + 1));
+	if (!line)
+		return (NULL);
+	counter = 0;
+	while (remainder[counter] != '\0' && remainder[counter] != '\n')
+	{
+		line[counter] = remainder[counter];
+		counter++;
+	}
+	if (remainder[counter] == '\n')
+		line[counter++] = '\n';
+	line[counter] = '\0';
+	return (line);
+}
+
+char	*remainder_refresh(char *remainder)
+{
+	char	*new_remainder;
+	int		counter;
+	int		new_counter;
+
+	counter = 0;
+	new_counter = 0;
+	if (remainder[counter] == '\0')
+		return (free(remainder), NULL);
+	while (remainder[counter] != '\0' && remainder[counter] != '\n')
+		counter++;
+	if (remainder[counter] == '\n')
+		counter++;
+	new_remainder = malloc(sizeof(char) * (ft_strlen(remainder) - counter + 1));
+	if (new_remainder == NULL)
+		return (free(remainder), NULL);
+	while (remainder[counter] != '\0')
+		new_remainder[new_counter++] = remainder[counter++];
+	new_remainder[new_counter] = '\0';
+	free(remainder);
+	return (new_remainder);
 }
